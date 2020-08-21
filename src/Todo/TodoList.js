9492ed97715d916e392fb89f2313570daf589ada
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-21 23:37:11 +0300

    components

---
+++ src/Todo/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+export default function TodoList() {
+    return (
+        <ul>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+        </ul>
+    )
+}
