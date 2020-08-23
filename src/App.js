593d0e59177991332dded151efc3ea6cd57db018
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-23 17:09:12 +0300

    dynamic classes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React from 'react'
 import TodoList from './Todo/TodoList'
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'React'},
     {id: 2, completed: false, title: 'Redux'},
     {id: 3, completed: false, title: 'Hooks'}
-  ]
+  ])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
 
   return (
       <div className="wrapper">
         <h1>React practice</h1>
 
-        <TodoList todos={todos} />
+        <TodoList
+         todos={todos} 
+         onToggle={toggleTodo}
+         />
 
       </div>
   );
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function TodoItem({ todo, index }) {
+function TodoItem({ todo, index, onChange }) {
+    const classes = []
+
+    if (todo.completed) {
+        classes.push('done')
+    }
+
     return (
         <li>
             <span>
-                <input type="checkbox" className="apple-switch" />
+                <input
+                 type="checkbox" 
+                 checked={todo.completed}
+                 //checkbox state
+                 className="apple-switch"
+                //  toggle
+                 onChange={() => onChange(todo.id)} 
+                 />
                 {/* <strong>{ index + 1 }</strong> */}
             </span>
 
+            <span className={classes.join('  ')}>
             { todo.title }
+            </span>
             
             <button className="btn-remove">Delete</button>
         </li>
@@ -18,7 +33,8 @@ function TodoItem({ todo, index }) {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired,
-    index: PropTypes.number
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem
