c578f8056835fc865cffe5018d23dd565991a3f3
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2020-08-26 18:34:54 +0300

    input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
+import AddTodo from './Todo/AddTodo'
 
 function App() {
   const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'React'},
     {id: 2, completed: false, title: 'Redux'},
-    {id: 3, completed: false, title: 'Hooks'}
+    {id: 3, completed: false, title: 'Hooks'},
+    {id: 4, completed: false, title: 'React JS + TypeScript(+ React Router DOM)'},
+    {id: 5, completed: false, title: 'React Native'}
   ])
 
   function toggleTodo(id) {
@@ -24,15 +27,21 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
   return (
     <Context.Provider value={{removeTodo: removeTodo}}>
         <div className="wrapper">
           <h1>React practice</h1>
-
-          <TodoList
-          todos={todos} 
-          onToggle={toggleTodo}
-          />
+          <AddTodo onCreate={addTodo} />
+          {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>no todos </p>}
+          
 
         </div>
       </Context.Provider>
